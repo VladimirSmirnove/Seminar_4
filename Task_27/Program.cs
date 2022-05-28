1b7e361094da20623284235a6e456fb6a2abd759
Vladimir <vladsmir323@gmail.com>
2022-05-28 08:15:13 +0300

    Программа для вывода на экран суммы цифр в числе

---
+++ Task_27/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 27: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе. Задачу решить без использования строк (string). (Решение задачи для положительных чисел)
+Console.WriteLine("Введите положительное число: ");
+int value = int.Parse(Console.ReadLine());
+int sumresult = 0;
+Console.Write($"В числе {value}");
+while (value > 0) //решение для положительных чисел
+{
+    sumresult += value % 10;
+    value /= 10;
+}
+Console.WriteLine($" cумма цифр = {sumresult}");
+
