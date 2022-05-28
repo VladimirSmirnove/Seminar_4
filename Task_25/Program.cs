7ea2d326f0ea788b8823ce76dc94558d4fd0d3cf
Vladimir <vladsmir323@gmail.com>
2022-05-28 08:01:46 +0300

    Возведение числа в натуральную степень

---
+++ Task_25/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число А в натуральную степень B. 
+//Задачу решить без использования Math Pow.
+Console.WriteLine("Введите число A: ");
+int value = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень B для числа A: ");
+int step = int.Parse(Console.ReadLine());
+int valuestep = 1;
+for (int i = 0; i < step; i++)
+{
+   valuestep *= value;
+}
+Console.WriteLine($"{value} ^ {step} = {valuestep}");
+ 
